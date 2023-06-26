1680728c09d79b498f7aff0dbf56ca68c9ec84b5
Edgar Esquivel <129891176+Emboesq13@users.noreply.github.com>
2023-06-25 18:40:09 -0600

    Add files via upload

---
+++ my-app/pro.sh
@@ -0,0 +1,12 @@
+gnome-terminal -- java -jar ./target/my-app-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+cd target/classes
+
+gnome-terminal -- java AuxServer 3001
+gnome-terminal -- java AuxServer 3002
+gnome-terminal -- java AuxServer 3003
+
+cd ..
+cd ..
+
+java -jar ./target/my-app-1.0-SNAPSHOT-jar-with-dependencies.jar
